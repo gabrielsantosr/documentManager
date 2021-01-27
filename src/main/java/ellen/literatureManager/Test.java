7b6648b6c629c3de29aef2bf204cbc34d9835681
Gabriel Santos <gabrielsantosr@gmail.com>
2021-01-27 12:31:46 +0000

    changes on Test, formerly TestDao

--- src/main/java/ellen/literatureManager/Test.java
+++ src/main/java/ellen/literatureManager/Test.java
@@ -1,29 +1,25 @@
 package ellen.literatureManager;
 
 import entities.Author;
-import entities.Authorship;
 import entities.Document;
 import entities.DocumentType;
 import service.DaoImpl;
+import service.Service;
 
-public class TestDAO {
-	private DaoImpl chePibe;
-	public TestDAO() {
-		this.chePibe = new DaoImpl();
-	}
+public class Test {
 	
 	public static void main(String []args) {
-		TestDAO test = new TestDAO();
-		Author author = (Author)test.chePibe.get(Author.class, 3);
+		Service service = new Service();
+		Author author = (Author)service.get(Author.class, 3, false);
 		System.out.println(author);
 		
-		test.chePibe.getAll(DocumentType.class)
+		service.getAll(DocumentType.class)
 			.forEach(row->{System.out.println(row);});
 	
-		Document doc = (Document)test.chePibe.getEager(Document.class,4);
+		Document doc = (Document)service.get(Document.class, 4, true);
 		//Note note = doc.getNotes().get(0);
 		//linker.update(note, new Note(doc,"Esta nota volvió a ser alterada"));
-		System.out.println(test.chePibe.get(6,5));
+		System.out.println(service.get(doc.getId(), author.getId()));
 				
 	}
 }
