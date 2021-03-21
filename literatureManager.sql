669e6d3036c0a0960df5943f5edd260f71f5c720
Gabriel Santos <gabrielsantosr@gmail.com>
2021-03-21 16:37:09 +0000

    Added more data to the .sql file

--- literatureManager.sql
+++ literatureManager.sql
@@ -27,7 +27,7 @@ CREATE TABLE `author` (
   `first_name` varchar(30) DEFAULT NULL,
   `last_name` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `author` (
 
 LOCK TABLES `author` WRITE;
 /*!40000 ALTER TABLE `author` DISABLE KEYS */;
-INSERT INTO `author` VALUES (1,'Ringo','Starr'),(2,'Jack','Santa Maria'),(3,'Oliver','Sacks'),(4,'Katie','Caldessi'),(5,'John','Lennon'),(6,'Giancarlo','Caldessi'),(7,'Joshua','Marinacci'),(8,'George','Harrison'),(9,'Chris','Adamson'),(10,'Paul','McCartney'),(11,'Gabriel','García Márquez'),(14,'Mario','Benedetti'),(15,'Hug','Penguin'),(16,'Bill','Bryson'),(17,'Paul','Atkinson'),(18,'Martyn','Hammersley'),(19,'Theodore','Bjerken'),(20,'Bea','Mello'),(21,'Rob','Mello'),(22,'Ellen','Casey'),(23,'Tim','Cacciatore'),(24,'Patrick','Johnson'),(25,'Rajal','Cohen'),(26,'Joan','Davies'),(27,'Carlos','Gallego-Cerveró'),(28,'Javier','Martín-Ruiz'),(29,'Laura','Ruiz-Sanchis');
+INSERT INTO `author` VALUES (1,'Ringo','Starr'),(2,'Jack','Santa Maria'),(3,'Oliver','Sacks'),(4,'Katie','Caldessi'),(5,'John','Lennon'),(6,'Giancarlo','Caldessi'),(7,'Joshua','Marinacci'),(8,'George','Harrison'),(9,'Chris','Adamson'),(10,'Paul','McCartney'),(11,'Gabriel','García Márquez'),(14,'Mario','Benedetti'),(15,'Hug','Penguin'),(16,'Bill','Bryson'),(17,'Paul','Atkinson'),(18,'Martyn','Hammersley'),(19,'Theodore','Bjerken'),(20,'Bea','Mello'),(21,'Rob','Mello'),(22,'Ellen','Casey'),(23,'Tim','Cacciatore'),(24,'Patrick','Johnson'),(25,'Rajal','Cohen'),(26,'Joan','Davies'),(27,'Clara','Gallego-Cerveró'),(28,'Julio','Martín-Ruiz'),(29,'Laura','Ruiz-Sanchis'),(30,'Concepción','Ros-Ros');
 /*!40000 ALTER TABLE `author` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +65,7 @@ CREATE TABLE `authorship` (
 
 LOCK TABLES `authorship` WRITE;
 /*!40000 ALTER TABLE `authorship` DISABLE KEYS */;
-INSERT INTO `authorship` VALUES (4,1,6),(1,2,2),(1,3,4),(1,3,5),(1,4,1),(1,5,6),(2,6,1),(1,7,3),(3,8,6),(2,9,3),(2,10,6),(1,17,7),(2,18,7),(1,19,8),(2,20,8),(3,21,8),(1,22,10),(1,23,9),(2,24,9),(3,25,9);
+INSERT INTO `authorship` VALUES (4,1,6),(1,2,2),(1,3,4),(1,3,5),(1,4,1),(1,5,6),(2,6,1),(1,7,3),(3,8,6),(2,9,3),(2,10,6),(1,17,7),(2,18,7),(1,19,8),(2,20,8),(3,21,8),(1,22,10),(1,23,9),(2,24,9),(3,25,9),(1,27,11),(2,28,11),(3,29,11),(4,30,11);
 /*!40000 ALTER TABLE `authorship` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +121,7 @@ CREATE TABLE `document` (
   UNIQUE KEY `title` (`title`,`vol_number`,`vol_release`,`year`),
   KEY `FK335CD11B9FCEBC5B` (`doc_type_id`),
   CONSTRAINT `FK335CD11B9FCEBC5B` FOREIGN KEY (`doc_type_id`) REFERENCES `doc_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,7 +130,7 @@ CREATE TABLE `document` (
 
 LOCK TABLES `document` WRITE;
 /*!40000 ALTER TABLE `document` DISABLE KEYS */;
-INSERT INTO `document` VALUES (1,1,NULL,NULL,'The long and the short of Pasta','A collection of treasured Italian dishes',NULL,NULL,NULL,'Hardie Grant Books','London',NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\pasta.jpg'),(2,1,NULL,NULL,'Indian vegetarian coockery',NULL,NULL,NULL,3,'Rider','London',NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\indian.jpg'),(3,1,NULL,NULL,'Swing Hacks','Tips & Tool for Building Killer GUIs',NULL,NULL,1,'O\'Reilly','Sebastopol, California',NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\swing_hacks.png'),(4,1,NULL,NULL,'Musicophilia',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\musicophilia.jpg'),(5,1,NULL,NULL,'The Man Who Mistook his Wife For A Hat',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\Soundbites.mp4'),(6,2,NULL,NULL,'Revolver',NULL,NULL,NULL,NULL,'EMI','London',NULL,NULL,NULL,'https://open.spotify.com/album/3PRoXYsngSwjEQWR5PsHWR?si=QZoGinDLREKGrsRoCtDqGQ'),(7,1,2007,NULL,'Ethnography:','Principles in practice',NULL,NULL,NULL,'Routledge','New York',NULL,NULL,NULL,NULL),(8,3,2012,'Theatre,Dance and Performance Training','Cultivating a lively use of tension:',' the synergy between acting and the AT','3','1',NULL,NULL,NULL,27,40,NULL,NULL),(9,3,2020,'Kinesiology Review','Potential Mechanics of the Alexander Technique:','towards a comprhensive neurophysiological model','9','3',NULL,NULL,NULL,199,213,NULL,NULL),(10,4,2020,NULL,'Exploring the perceived impact of extensive training in the Alexander technique on the day-to-day lives of professional musicians',NULL,NULL,NULL,NULL,'Royal College of Music','London',NULL,NULL,NULL,NULL);
+INSERT INTO `document` VALUES (1,1,NULL,NULL,'The long and the short of Pasta','A collection of treasured Italian dishes',NULL,NULL,NULL,'Hardie Grant Books','London',NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\pasta.jpg'),(2,1,NULL,NULL,'Indian vegetarian coockery',NULL,NULL,NULL,3,'Rider','London',NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\indian.jpg'),(3,1,NULL,NULL,'Swing Hacks','Tips & Tool for Building Killer GUIs',NULL,NULL,1,'O\'Reilly','Sebastopol, California',NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\swing_hacks.png'),(4,1,NULL,NULL,'Musicophilia',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\musicophilia.jpg'),(5,1,NULL,NULL,'The Man Who Mistook his Wife For A Hat',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'C:\\Users\\gabri\\OneDrive\\Escritorio\\Soundbites.mp4'),(6,2,NULL,NULL,'Revolver',NULL,NULL,NULL,NULL,'EMI','London',NULL,NULL,NULL,'https://open.spotify.com/album/3PRoXYsngSwjEQWR5PsHWR?si=QZoGinDLREKGrsRoCtDqGQ'),(7,1,2007,NULL,'Ethnography:','Principles in practice',NULL,NULL,NULL,'Routledge','New York',NULL,NULL,NULL,NULL),(8,3,2012,'Theatre,Dance and Performance Training','Cultivating a lively use of tension:',' the synergy between acting and the AT','3','1',NULL,NULL,NULL,27,40,NULL,NULL),(9,3,2020,'Kinesiology Review','Potential Mechanics of the Alexander Technique:','towards a comprhensive neurophysiological model','9','3',NULL,NULL,NULL,199,213,NULL,NULL),(10,4,2020,NULL,'Exploring the perceived impact of extensive training in the Alexander technique on the day-to-day lives of professional musicians',NULL,NULL,NULL,NULL,'Royal College of Music','London',NULL,NULL,NULL,NULL),(11,3,2018,'Medical Problems of Performing Artists','Pain perception in clarinetists with Playing-related pain after implementing a Specific Exercise Program',NULL,NULL,NULL,NULL,NULL,NULL,238,242,'https://doi.org/10.21091/mppa.2018.4035','C:\\Users\\gabri\\OneDrive\\Escritorio\\Cervero.pdf');
 /*!40000 ALTER TABLE `document` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -170,4 +170,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-16 10:48:50
+-- Dump completed on 2021-03-21 16:32:22
